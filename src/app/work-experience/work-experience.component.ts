a8742b593a33e667e470a43344c24da22810d9b6
JAntonio2005 <joseantinioortizhernandez777@gmail.com>
2025-03-12 16:09:27 -0600

    Subi los componenetes header, work-experience

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {WorkExperienceService} from "../services/work-experience-service/work-experience.service";
 
 @Component({
   selector: 'app-work-experience',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './work-experience.component.css'
 })
 export class WorkExperienceComponent {
-
+constructor (public workExperienceService: WorkExperienceService){
+console.log(this.workExperienceService);
+}
 }
